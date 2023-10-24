cd5cf510cc021b311a98a5e7ff65fc594584d5bb
pedrosamegima <136722484+pedrosamegima@users.noreply.github.com>
2023-10-24 10:37:29 -0300

    Add files via upload

---
+++ ProjetoLivro/src/main/java/com/projetoLivro/ProjetoLivroApplication.java
@@ -0,0 +1,13 @@
+package com.projetoLivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLivroApplication.class, args);
+	}
+
+}
